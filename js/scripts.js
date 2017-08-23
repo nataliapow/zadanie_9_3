3499f44bc833bddd64ff502d28cba70b9ac48e0a
Natalia <n.powalska@gmail.com>
2017-08-23 14:16:03 +0200

    Add comma separator to a value variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
-var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
-	dinosaur = 'triceratops';
-	dinosaurUpperCased = dinosaur.toUpperCase();
-	textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased);
-	halfSentence = textCharsAfter.length/2;
-
-console.log(halfSentence);
-
-	halfText = textCharsAfter.slice(0, halfSentence);
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.",
+	dinosaur = 'triceratops',
+	dinosaurUpperCased = dinosaur.toUpperCase(),
+	textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased),
+	halfSentence = textCharsAfter.length/2,
+	halfText = textCharsAfter.slice(0, halfSentence),
 
 console.log(halfText);
